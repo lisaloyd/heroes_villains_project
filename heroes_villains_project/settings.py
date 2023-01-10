d592afb90c2b3cf614a30989f4f557c2add6eb13
unknown <greateducation123@yahoo.com>
2023-01-10 14:07:39 -0600

    Created new apps supers and super_types + created their models + downloaded the Postman collection from the online portal

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -35,7 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'rest_framework',
+    'supers',
+    'super_types',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
