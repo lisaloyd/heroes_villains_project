e679c98cd133127350a05ba1b1acd776979fa8ee
unknown <greateducation123@yahoo.com>
2023-01-10 17:16:21 -0600

    Checked the url path for supers in Postman + serialized more in views

--- supers/serializers.py
+++ supers/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Super
 
-class Superserializer(serializers.modelSerializer):
+class SuperSerializer(serializers.ModelSerializer):
     class Meta:
         model = Super
         fields = ['id','name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
--- supers/views.py
+++ supers/views.py
@@ -1,8 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SuperSerializer
+from .models import Super
 
 @api_view(['GET'])
 def supers_list(request):
+    supers = Super.objects.all()
 
+    serializer = SuperSerializer(supers, many=True)
 
-    return Response('ok')
+    return Response(serializer.data)
