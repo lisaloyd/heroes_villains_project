fa6f6fbd405b48d27f41fe02d874a19767832351
unknown <greateducation123@yahoo.com>
2023-01-11 16:14:44 -0600

    Created a PUT endpoint that accepts a value from the request's url + returs a 200 status code + responds with the new updated super object

--- supers/views.py
+++ supers/views.py
@@ -19,10 +19,14 @@ def supers_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def super_detail(request, pk): 
     super = get_object_or_404(Super, pk=pk)
-    serializer = SuperSerializer(super);
-    return Response(serializer.data)
-
-   
+    if request.method == 'GET':
+        serializer = SuperSerializer(super);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
